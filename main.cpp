fe13a211f0ec01c86bd224d59df05ae5dbb9c4f1
chloemb <43690200+chloemb@users.noreply.github.com>
2018-10-08 12:59:44 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <math.h>
+#include <list>
+#include <iterator>
 
 using namespace std;
 
@@ -15,6 +17,13 @@ using namespace std;
  6 cha
  */
 
+void showlist(list <int> g)
+{
+    list <int> :: iterator it;
+    for (it = g.begin(); it != g.end(); it++) {
+        cout << " - " << *it;
+    }
+}
 
 int main() {
     srand((int)time(NULL));     //initialize random seed
@@ -23,7 +32,7 @@ int main() {
     
     //roll stats one time
     
-    int n = 1;
+    int n = 20;
     
     
     //give a prompt to enter how many times Lunatic should be rolled
@@ -34,9 +43,17 @@ int main() {
         cout << "Invalid input" << endl;
         return 0;
     }
-     */
+    */
     
     double trolls = 0, tSTR = 0, tDEX = 0, tCON = 0, tINT = 0, tWIS = 0, tCHA = 0;
+    
+    int lSTR = 21, hSTR = 0, lDEX = 21, hDEX = 0, lCON = 21, hCON = 0, lINT = 21, hINT = 0, lWIS = 21, hWIS = 0, lCHA = 21, hCHA = 0;
+    
+    double lAVG = 21, hAVG = 0;
+    
+    list <int> lSTRr, hSTRr, lDEXr, hDEXr, lCONr, hCONr, lINTr, hINTr, lWISr, hWISr, lCHAr, hCHAr, hAVGr, lAVGr;
+    
+    int* statsArray = new int[7];
     
     double *STR = new double, *DEX = new double, *CON = new double, *INT = new double, *WIS = new double, *CHA = new double, *sk = new double;
     
@@ -52,9 +69,9 @@ int main() {
             d6 = (rand() % 6) + 1;
             
             //print each d20 and d6 set
-            
+            /*
              printf("Loop %d: d20 rolled %d, d6 rolled %d\n", xloop, d20, d6);
-            
+            */
             
             switch (d6) {
                 case 1:
@@ -88,12 +105,13 @@ int main() {
             
         }
         
+        double avg = (*STR + *DEX + *CON + *INT + *WIS + *CHA) / 6;
         
         //print stats and roll number for one set
         
         cout << endl;
-        cout << "STR: " << *STR << ", DEX: " << *DEX << ", CON: " << *CON << ", INT: " << *INT << ", WIS: " << *WIS << ", CHA: " << *CHA << endl;
-        cout << "Rolled " << xloop - 1 << " times" << endl;
+        cout << "(" << i + 1 << ") " << "STR: " << *STR << ", DEX: " << *DEX << ", CON: " << *CON << ", INT: " << *INT << ", WIS: " << *WIS << ", CHA: " << *CHA << ", average: " << avg << endl;
+        //cout << "Rolled " << xloop - 1 << " times" << endl;
         
         
         //print stats as raw data w/tab in between
@@ -102,7 +120,6 @@ int main() {
          */
         
         //total sum of each respective stat rolled over n stat sets
-        /*
         trolls += (xloop - 1);
         tSTR += *STR;
         tDEX += *DEX;
@@ -110,7 +127,121 @@ int main() {
         tINT += *INT;
         tWIS += *WIS;
         tCHA += *CHA;
-         */
+        
+        //keep track of lows and highs for each score
+        
+        if (*STR <= lSTR) {
+            if (*STR < lSTR) {
+                lSTR = *STR;
+                lSTRr.clear();
+            }
+            lSTRr.push_back(i + 1);
+        }
+        
+        if (*STR >= hSTR) {
+            if (*STR > hSTR) {
+                hSTR = *STR;
+                hSTRr.clear();
+            }
+            hSTRr.push_back(i + 1);
+        }
+        
+        if (*DEX <= lDEX) {
+            if (*DEX < lDEX) {
+                lDEX = *DEX;
+                lDEXr.clear();
+            }
+            lDEXr.push_back(i + 1);
+        }
+        
+        if (*DEX >= hDEX) {
+            if (*DEX > hDEX) {
+                hDEX = *DEX;
+                hDEXr.clear();
+            }
+            hDEXr.push_back(i + 1);
+        }
+        
+        if (*CON <= lCON) {
+            if (*CON < lCON) {
+                lCON = *CON;
+                lCONr.clear();
+            }
+            lCONr.push_back(i + 1);
+        }
+        
+        if (*CON >= hCON) {
+            if (*CON > hCON) {
+                hCON = *CON;
+                hCONr.clear();
+            }
+            hCONr.push_back(i + 1);
+        }
+        
+        if (*INT <= lINT) {
+            if (*INT < lINT) {
+                lINT = *INT;
+                lINTr.clear();
+            }
+            lINTr.push_back(i + 1);
+        }
+        
+        if (*INT >= hINT) {
+            if (*INT > hINT) {
+                hINT = *INT;
+                hINTr.clear();
+            }
+            hINTr.push_back(i + 1);
+        }
+        
+        if (*WIS <= lWIS) {
+            if (*WIS < lWIS) {
+                lWIS = *WIS;
+                lWISr.clear();
+            }
+            lWISr.push_back(i + 1);
+        }
+        
+        if (*WIS >= hWIS) {
+            if (*WIS > hWIS) {
+                hWIS = *WIS;
+                hWISr.clear();
+            }
+            hWISr.push_back(i + 1);
+        }
+        
+        if (*CHA <= lCHA) {
+            if (*CHA < lCHA) {
+                lCHA = *CHA;
+                lCHAr.clear();
+            }
+            lCHAr.push_back(i + 1);
+        }
+        
+        if (*CHA >= hCHA) {
+            if (*CHA > hCHA) {
+                hCHA = *CHA;
+                hCHAr.clear();
+            }
+            hCHAr.push_back(i + 1);
+        }
+        
+        if (avg <= lAVG) {
+            if (avg < lAVG) {
+                lAVG = avg;
+                lAVGr.clear();
+            }
+            lAVGr.push_back(i + 1);
+        }
+        
+        if (avg >= hAVG) {
+            if (avg > hAVG) {
+                hAVG = avg;
+                hAVGr.clear();
+            }
+            hAVGr.push_back(i + 1);
+        }
+        
     }
     
     //calculate the average and print
@@ -121,7 +252,71 @@ int main() {
     cout << "Average of average stats: " << (tSTR/n + tDEX/n + tCON/n + tINT/n + tWIS/n + tCHA/n)/6 << endl;
     cout << endl;
     cout << "Average number of rolls: " << trolls/n << endl;
-    */
+     */
+    
+    //highs and lows of each score as well as average score and which loop they were rolled on
+    
+    cout << endl;
+    cout << "Additional information:" << endl;
+    
+    cout << "High average: " << hAVG << " (loop";
+    showlist(hAVGr);
+    cout << ")" << endl;
+    
+    cout << "High STR: " << hSTR << " (loop";
+    showlist(hSTRr);
+    cout << ")" << endl;
+    
+    cout << "High DEX: " << hDEX << " (loop";
+    showlist(hDEXr);
+    cout << ")" << endl;
+    
+    cout << "High CON: " << hCON << " (loop";
+    showlist(hCONr);
+    cout << ")" << endl;
+    
+    cout << "High INT: " << hINT << " (loop";
+    showlist(hINTr);
+    cout << ")" << endl;
+    
+    cout << "High WIS: " << hWIS << " (loop";
+    showlist(hWISr);
+    cout << ")" << endl;
+    
+    cout << "High CHA: " << hCHA << " (loop";
+    showlist(hCHAr);
+    cout << ")" << endl;
+    
+    cout << endl;
+    
+    cout << "Low average: " << lAVG << " (loop";
+    showlist(lAVGr);
+    cout << ")" << endl;
+    
+    cout << "Low STR: " << lSTR << " (loop";
+    showlist(lSTRr);
+    cout << ")" << endl;
+    
+    cout << "Low DEX: " << lDEX << " (loop";
+    showlist(lDEXr);
+    cout << ")" << endl;
+    
+    cout << "Low CON: " << lCON << " (loop";
+    showlist(lCONr);
+    cout << ")" << endl;
+    
+    cout << "Low INT: " << lINT << " (loop";
+    showlist(lCONr);
+    cout << ")" << endl;
+    
+    cout << "Low WIS: " << lWIS << " (loop";
+    showlist(lWISr);
+    cout << ")" << endl;
+    
+    cout << "Low CHA: " << lCHA << " (loop";
+    showlist(lCHAr);
+    cout << ")" << endl;
+
     
     delete STR;
     delete DEX;
